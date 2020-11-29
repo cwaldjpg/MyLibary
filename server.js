88aa0f5d2a0871d2f3778d8d85d6aa3ef3c870a9
Hieu Nguyen <kerituni123@gmail.com>
2020-11-29 16:10:50 +0700

    rating

--- server.js
+++ server.js
@@ -1,69 +1,76 @@
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
-const expressSession = require('express-session')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
-const passport = require('passport')
-const flash = require('connect-flash')
-const _ = require('lodash')
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const expressSession = require("express-session");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const flash = require("connect-flash");
+const _ = require("lodash");
 
-const app = express()
+const app = express();
 
-require('./config/passport')(passport)
+require("./config/passport")(passport);
 
-const indexRouter = require('./routes/index')
-const userRouter = require('./routes/users')
-const authorRouter = require('./routes/authors')
-const bookRouter = require('./routes/books')
-const locationRouter = require('./routes/locations')
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/users");
+const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
+const locationRouter = require("./routes/locations");
 
-mongoose.connect('mongodb+srv://admin:admin@mylibary.jt0cq.mongodb.net/MyLibary?retryWrites=true&w=majority', {useNewUrlParser: true})
-.then(result => {
-  console.log('connect to DB')
-})
-.catch(error => {
-  console.log('***'+error)
-})
+mongoose
+  .connect("mongodb+srv://admin:admin@mylibary.jt0cq.mongodb.net/MyLibary?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+  })
+  .then((result) => {
+    console.log("connect to DB");
+  })
+  .catch((error) => {
+    console.log("***" + error);
+  });
 
-app.set('view engine','ejs')
-app.set('views', __dirname + '/views')
-app.set('layout','layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({limit:'10mb', extended: false}))
-app.use(methodOverride('_method'))
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+app.use(methodOverride("_method"));
 
-app.use(expressSession({    
-  secret:'this is a session',    
-  resave: false,
-  saveUninitialized: false
-}))
+app.use(bodyParser.json());
 
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(
+  expressSession({
+    secret: "this is a session",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(flash());
-app.use((req,res,next) => {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   if (!res.locals.currentUser) {
-    if (_.has(req, 'session.passport.user')) {
-      res.locals.currentUser = req.session.passport.user
+    if (_.has(req, "session.passport.user")) {
+      res.locals.currentUser = req.session.passport.user;
     } else {
       res.locals.currentUser = null;
     }
   }
   next();
-})
+});
 
-app.use('/', indexRouter)
-app.use('/users', userRouter)
-app.use('/authors', authorRouter)
-app.use('/books', bookRouter)
-app.use('/locations', locationRouter)
+app.use("/", indexRouter);
+app.use("/users", userRouter);
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
+app.use("/locations", locationRouter);
 
-app.listen(3000,() => {
-    console.log('Listening to port 3000')
-})
+app.listen(3000, () => {
+  console.log("Listening to port 3000");
+});
